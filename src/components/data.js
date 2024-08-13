2e3dce3c4b1d60c1c1df60794280dafcad3b79e6
Saksham Soni <110776016+sakshams23@users.noreply.github.com>
2024-08-13 12:04:18 +0530

    Update data.js

--- src/components/data.js
+++ src/components/data.js
@@ -2,11 +2,11 @@
 const cardData = [
     {
       id: 1,
-      videoSrc: require('../assets/v1.mp4'), // Update with actual paths
+      videoSrc: require('../assets/v1.mp4'), 
       title: 'Elderly Ways',
       description: 'This is a Fundraiser web application made using React.js, Tailwind CSS connecting contributors with NGOs and senior citizens in need. Currently adding backend using Express.',
-      link1: 'https://github.com/sakshams23/ElderlyWays',
-      link2: 'https://elderlyways.vercel.app/',
+      link1: 'https://github.com/sakshams23/Elderly_ways',
+      link2: 'https://elderly-wayss.vercel.app/',
     },
     {
       id: 2,
@@ -21,8 +21,24 @@ const cardData = [
       videoSrc: require('../assets/v3.mp4'),
       title: 'Nexgen Digital Solutions',
       description: 'Collaborated with a team to build the official website of the company using MERN stack and Optimized backend services and API endpoints for improved performance and scalability.',
-      link1: '',
-      link2: '',
+      link1: 'https://github.com/sakshams23/Nexgen-Digital-Solutions',
+      link2: 'https://nexgen-digital-solutions.vercel.app/',
+    },
+    {
+      id: 4,
+      videoSrc: require('../assets/v5.mp4'),
+      title: 'Portfolio Website',
+      description: 'A portfolio website built with React and Tailwind CSS offers a modern and dynamic way to showcase your skills and projects. Utilizing React component-based architecture, the site is highly responsive, ensuring smooth navigation and fast load times.',
+      link1: 'https://github.com/sakshams23/MyPortfolio',
+      link2: 'https://my-portfolio-cj47.vercel.app/',
+    },
+    {
+      id: 5,
+      videoSrc: require('../assets/v4.mp4'),
+      title: 'Alarm Clock',
+      description: 'It is a functional web-based application built using HTML, CSS, and JavaScript. This project allows users to set an alarm for a specific time and notifies them with a sound when the set time is reached.',
+      link1: 'https://github.com/sakshams23/Alarm_Clock-using_Js-',
+      link2: 'https://alarm-clock-using-js.vercel.app/',
     },
     
     // Add more card data objects as needed
