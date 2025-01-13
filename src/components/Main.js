f0885166b88e014e4df9efd207992bd808a976a3
Saksham Soni <sonisaksham20208@gmail.com>
2025-01-13 22:03:35 +0530

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,8 +6,8 @@ export class Main extends Component {
     return (
         <div className=" h-full flex flex-col items-start justify-evenly laptop:px-28 laptop:pt-40  text-black phone:px-18 phone:pt-28 phone:px-5">
         <ul className=" list-none">
-            <li className=" laptop:font-semibold laptop:text-2xl laptop:mb-3 phone:text-xl phone:mb-1  ">Hi! My name is Saksham Soni</li>
-            <li className=" laptop:text-2xl laptop:font-medium phone:text-lg">
+            <li className=" laptop:font-semibold laptop:text-3xl laptop:mb-3 phone:text-xl phone:mb-1  ">Hi! My name is Saksham Soni</li>
+            <li className=" laptop:text-3xl laptop:font-medium phone:text-xl">
         <ReactTyped
           strings={["Full Stack Developer", "Competitive Programmer"]}
           typeSpeed={100}
@@ -16,7 +16,7 @@ export class Main extends Component {
           cursorChar=">"
           showCursor={true}
         /></li>
-            <li className=" laptop:mt-3 laptop:text-xl phone:text-sm">
+            <li className=" laptop:mt-3 laptop:text-lg phone:text-sm">
                 As a passionate and driven individual, I am currently pursuing a Bachelor’s degree in Electronics Engineering from Kamla Nehru Institute of Technology Sultanpur, with an expected graduation in May 2025. My strong enthusiasm for technology and problem-solving has led me to master programming languages like Java, C++, JavaScript, and Python, and to develop proficiency in modern web technologies, particularly the MERN stack. <br/> With expertise in Machine Learning and Artificial Intelligence, I have honed my skills in leveraging tools like Pandas, Scikit-learn, and TensorFlow to build intelligent, data-driven solutions. I am proficient in working with Conda for efficient environment management and have experience using Langchain to develop innovative solutions in natural language processing and AI-driven applications. Additionally, I am a Salesforce Certified AI Associate, showcasing my commitment to staying updated with the latest advancements in AI technologies and industry practices. <br/> I thrive in collaborative environments, actively seeking opportunities to learn from peers and contribute to innovative solutions. I am always eager to discuss programming, web development, AI, and emerging technologies that drive impactful change.
             </li>
             <li className="mt-6 laptop:text-lg phone:text-sm">
